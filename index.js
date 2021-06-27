a69a7c252d8546c8cae0972f943e7f9e07f7b18c
kiranbodicherla <kiranbits199@gmail.com>
2021-06-27 23:20:28 +0530

    Enter

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 let expression="";
-let list;
-let input=document.getElementById("input");
-let buttons=document.querySelectorAll("button");
-
-for(list of buttons){
-    list.addEventListener("click" ,(e) =>{
-
-        let inputTex=e.target.innerText;
-
-        console.log("inner text is" +inputTex);
-
-        if(inputTex=='X'){
-            inputTex='*';
-
-            expression+=inputTex;
-            input.value=expression;
-        }
-        else if(inputTex=='C'){
-            expression="";
-            input.value=expression;
-        }
-        else if(inputTex=='='){
-            input.value=eval(expression);
-        }
-        else{
-            expression+=inputTex;
-            input.value=expression;    
-        }
-    })
-}
-
+function add(x)
+    {
+        
+      document.getElementById('input').value+= x;
+    }
+    function solve()
+    {
+      
+     let x= document.getElementById('input').value
+      let b=eval(x);
+      document.getElementById('input').value=b;
+    }
+    function clr()
+    {
+        expression=" ";
+      document.getElementById('input').value=" ";
+    }
 
 
 
